6e3d1078a49840aa1a48aecfef34bbf1a804ccd2
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-03-24 12:43:37 -0300

    185. Crear un widget independiente para el drawer

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:preferenciasusuario/src/pages/settings_page.dart';
+
+import 'package:preferenciasusuario/src/widgets/menu_widget.dart';
 
 class HomePage extends StatelessWidget {
   static final String routeName = 'home';
@@ -11,7 +12,7 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         title: Text('Preferencias de Usuario'),
       ),
-      drawer: _crearMenu(context),
+      drawer: MenuWidget(),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -21,49 +22,6 @@ class HomePage extends StatelessWidget {
           Divider(),
           Text('Nombre usuario'),
           Divider(),
-        ],
-      ),
-    );
-  }
-
-  Drawer _crearMenu(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            child: Container(),
-            decoration: BoxDecoration(
-                image: DecorationImage(
-              image: AssetImage('assets/menu-img.jpg'),
-              fit: BoxFit.cover,
-            )),
-          ),
-          ListTile(
-            leading: Icon(Icons.pages, color: Colors.blue),
-            title: Text('Home'),
-            onTap: () =>
-                Navigator.pushReplacementNamed(context, HomePage.routeName),
-          ),
-          ListTile(
-            leading: Icon(Icons.party_mode, color: Colors.blue),
-            title: Text('Party Mode'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.people, color: Colors.blue),
-            title: Text('People'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.settings, color: Colors.blue),
-            title: Text('Settings'),
-            onTap: () {
-              // Navigator.pop(context);
-              // Navigator.pushNamed(context, SettingsPage.routeName);
-              Navigator.pushReplacementNamed(context, SettingsPage.routeName);
-            },
-          ),
         ],
       ),
     );
--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:preferenciasusuario/src/widgets/menu_widget.dart';
+
 class SettingsPage extends StatelessWidget {
   static final String routeName = 'settings';
 
@@ -9,6 +11,7 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Ajustes'),
       ),
+      drawer: MenuWidget(),
       body: Center(
         child: Text('Ajustes Page'),
       ),
