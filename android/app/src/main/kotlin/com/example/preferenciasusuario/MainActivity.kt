79572752dc04e294ea280a208c6bc7c842cb2eba
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-03-24 12:04:00 -0300

    183. Inicio de proyecto - Preferencias de Usuario

---
+++ android/app/src/main/kotlin/com/example/preferenciasusuario/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.preferenciasusuario
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static final String routeName = 'home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preferencias de Usuario'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Color secundario'),
+          Divider(),
+          Text('Genero'),
+          Divider(),
+          Text('Nombre usuario'),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
